14049e2fc7109e3e7ccaeb0ff070c80352cb6570
kaua <kaua.santanaj@gmail.com>
2026-01-21 21:00:23 -0300

    first commit

---
+++ src/main/java/kaua/recisao/RecisaoApplication.java
@@ -0,0 +1,18 @@
+package kaua.recisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecisaoApplication.class, args);
+
+
+
+	}
+
+
+
+}
---
+++ src/test/java/kaua/recisao/RecisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package kaua.recisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
