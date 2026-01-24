c00564438d28f296db74595d182b8dc9dabb73aa
kaua <kaua.santanaj@gmail.com>
2026-01-23 21:07:49 -0300

    modify method in user model

--- src/main/java/kaua/recisao/config/TokenConfig.java
+++ src/main/java/kaua/recisao/config/TokenConfig.java
@@ -1,12 +1,29 @@
 package kaua.recisao.config;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import kaua.recisao.entity.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TokenConfig {
 
+    @Value("{jwt.secret}")
+    private String secret;
 
 
+
+    public String generateToken(User user){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        try {
+            String token = JWT.create()
+                    .withClaim("UserName " + user.getUsername())
+        }
+
+    }
 
 
 
