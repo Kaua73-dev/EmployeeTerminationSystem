4149720344906c3fb76e5fd3ff6961bf9038774d
kaua <kaua.santanaj@gmail.com>
2026-01-24 15:39:36 -0300

    add user controller

---
+++ src/main/java/kaua/recisao/controller/UserController.java
@@ -0,0 +1,10 @@
+package kaua.recisao.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+}
