80c4b960c5be21e6b6c3190fb1f4b2940b53b6f6
kaua <kaua.santanaj@gmail.com>
2026-01-24 16:59:39 -0300

    add service login user

--- src/main/java/kaua/recisao/service/user/UserService.java
+++ src/main/java/kaua/recisao/service/user/UserService.java
@@ -1,11 +1,19 @@
 package kaua.recisao.service.user;
 
 
+import kaua.recisao.config.SecurityConfig;
+import kaua.recisao.config.TokenConfig;
+import kaua.recisao.dto.request.user.UserLoginRequest;
 import kaua.recisao.dto.request.user.UserRegisterRequest;
+import kaua.recisao.dto.response.user.UserLoginResponse;
 import kaua.recisao.dto.response.user.UserRegisterResponse;
 import kaua.recisao.entity.model.User;
 import kaua.recisao.entity.repository.UserRepository;
 import kaua.recisao.exceptions.user.UserAlreadyExistException;
+import kaua.recisao.exceptions.user.UserNotFoundException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +22,14 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final TokenConfig tokenConfig;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, TokenConfig tokenConfig) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.tokenConfig = tokenConfig;
     }
 
     public UserRegisterResponse register(UserRegisterRequest request){
@@ -40,5 +52,22 @@ public class UserService {
         );
 
     }
+
+    public UserLoginResponse login(UserLoginRequest request){
+        if(userRepository.findByCpf(request.cpf()).isEmpty()){
+            throw new UserNotFoundException();
+        }
+
+        UsernamePasswordAuthenticationToken  userAndPass = new UsernamePasswordAuthenticationToken(request.cpf(), request.password());
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authentication.getPrincipal();
+        String token = tokenConfig.generateToken(user);
+        return new UserLoginResponse(token);
+
+
+    }
+
+
 
 }
