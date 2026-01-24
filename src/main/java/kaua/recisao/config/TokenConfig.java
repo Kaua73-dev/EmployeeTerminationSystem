bc7793ff98fe97b28166b9e502ba51f920678dac
kaua <kaua.santanaj@gmail.com>
2026-01-23 21:16:27 -0300

    add method generate token and validateToken

--- src/main/java/kaua/recisao/config/TokenConfig.java
+++ src/main/java/kaua/recisao/config/TokenConfig.java
@@ -2,9 +2,16 @@ package kaua.recisao.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import kaua.recisao.entity.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
 
 @Component
 public class TokenConfig {
@@ -20,13 +27,36 @@ public class TokenConfig {
 
         try {
             String token = JWT.create()
-                    .withClaim("UserName ", user.getUsername())
-                    .withSubject(user.get)
+                    .withClaim("UserName ", user.getName())
+                    .withSubject(user.getCpf())
+                    .withExpiresAt(genExpiration())
+                    .withIssuedAt(Instant.now())
+                    .sign(algorithm);
+                    return token;
+
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error while generate Token", exception);
         }
 
     }
 
 
+    public Instant genExpiration(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+                return "";
+        }
+    }
 
 }
