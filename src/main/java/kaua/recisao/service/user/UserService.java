d656212a6dba1fe28fc352b4f10ae859f732c3c8
kaua <kaua.santanaj@gmail.com>
2026-01-24 16:27:04 -0300

    add service register user

--- src/main/java/kaua/recisao/service/user/UserService.java
+++ src/main/java/kaua/recisao/service/user/UserService.java
@@ -3,17 +3,21 @@ package kaua.recisao.service.user;
 
 import kaua.recisao.dto.request.user.UserRegisterRequest;
 import kaua.recisao.dto.response.user.UserRegisterResponse;
+import kaua.recisao.entity.model.User;
 import kaua.recisao.entity.repository.UserRepository;
 import kaua.recisao.exceptions.user.UserAlreadyExistException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserRegisterResponse register(UserRegisterRequest request, String cpf){
@@ -21,6 +25,17 @@ public class UserService {
             throw new UserAlreadyExistException();
         }
 
+        User user = new User();
+        user.setName(request.name());
+        user.setCpf(request.cpf());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setProvider(request.provider());
+
+        return new UserRegisterResponse(
+          user.getName(),
+          user.getCpf(),
+          user.getProvider()
+        );
 
     }
 
