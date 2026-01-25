3dfa3eadf8ee371da9493c627133fbc048feac98
kaua <kaua.santanaj@gmail.com>
2026-01-25 14:13:50 -0300

    add authoritie for login

--- src/main/java/kaua/recisao/config/SecurityConfig.java
+++ src/main/java/kaua/recisao/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(securityFilterConfig, UsernamePasswordAuthenticationFilter.class)
                 .build();
