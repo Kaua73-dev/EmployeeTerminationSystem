0bf130f5b8dfab1f4145a8bc4469b14e87ac317b
kaua <kaua.santanaj@gmail.com>
2026-01-27 15:48:51 -0300

    add employee termination repository

---
+++ src/main/java/kaua/recisao/entity/repository/EmployeeTerminationRepository.java
@@ -0,0 +1,12 @@
+package kaua.recisao.entity.repository;
+
+import kaua.recisao.entity.model.EmployeeTermination;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeTerminationRepository extends JpaRepository<EmployeeTermination, Integer> {
+
+
+
+}
