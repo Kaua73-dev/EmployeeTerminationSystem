0d93006ad5aa2594e57b59e007f3473ca03d9d25
kaua <kaua.santanaj@gmail.com>
2026-01-28 17:33:23 -0300

    add controller for createEmployee

--- src/main/java/kaua/recisao/controller/employeeTermination/EmployeeTerminationController.java
+++ src/main/java/kaua/recisao/controller/employeeTermination/EmployeeTerminationController.java
@@ -1,10 +1,34 @@
 package kaua.recisao.controller.employeeTermination;
 
 
+import kaua.recisao.dto.request.EmployeeTermination.EmployeeTerminationRequest;
+import kaua.recisao.dto.response.EmployeeTermination.EmployeeTerminationResponse;
+import kaua.recisao.service.EmployeeTermination.EmployeeTerminationService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class EmployeeTerminationController {
+
+
+    private final EmployeeTerminationService employeeTerminationService;
+
+    public EmployeeTerminationController(EmployeeTerminationService employeeTerminationService) {
+        this.employeeTerminationService = employeeTerminationService;
+    }
+
+
+
+    @PostMapping("employeeTermination")
+    public EmployeeTerminationResponse createEmployee(@RequestBody EmployeeTerminationRequest request){
+        return employeeTerminationService.createEmployeeTermination(request);
+    }
+
+
+
+
+
 }
